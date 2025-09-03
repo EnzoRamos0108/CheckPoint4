303534f4731591ff26510a62bc4b7e60c8011a92
Felipe <fecerazi@gmail.com>
2025-09-03 19:36:03 -0300

    Criando rotas no backend usando express

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,15 @@ app.listen(port, ()=>{
 })
 app.use(express.json())
 app.use(cors())
+
+app.get('/carros', (req, res) => {
+  res.json([{ id: 1, nome: 'Fusca' }, { id: 2, nome: 'Mustang' }]);
+});
+
+app.get('/sobre', (req, res) => {
+  res.json({ texto: 'Somos apaixonados por carros!' });
+});
+
+app.get('/contato', (req, res) => {
+  res.json({ email: 'contato@motorhorses.com', telefone: '(11) 95299-2081' });
+});
